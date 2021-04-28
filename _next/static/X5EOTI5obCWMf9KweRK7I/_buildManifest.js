a9537fe841a0d55a27ffd6d5301f294795d789fd
tabs-not-spaces <33951277+tabs-not-spaces@users.noreply.github.com>
2021-04-28 07:55:41 +0000

    Deploying to gh-pages from  @ 656b2bac61f74a702392fad28ff2e59db59bb13c 🚀

---
+++ _next/static/X5EOTI5obCWMf9KweRK7I/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-d19e8a31af04002d5ecf.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-c2bba75d0cb2ebc6486a.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
