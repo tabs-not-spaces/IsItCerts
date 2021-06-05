f5d27c8c4bdc8f2be75830075f026f69ed6f2527
Ben R <33951277+tabs-not-spaces@users.noreply.github.com>
2021-06-05 19:16:39 +1000

    Update index.tsx
    adding tracking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -103,6 +103,15 @@ export default function Home() {
   return (
     <div>
       <Head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-YD5FQFDKCT"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-YD5FQFDKCT');
+        </script>
         <title>Is It Certificates?</title>
       </Head>
 
