9c6fc71538d478648b3d3ce6a91159ed0ca6ea92
Ben R <33951277+tabs-not-spaces@users.noreply.github.com>
2021-06-14 19:16:27 +1000

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -104,6 +104,15 @@ export default function Home() {
     <div>
       <Head>
         <title>Is It Certificates?</title>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1CKSSLG3T4"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-1CKSSLG3T4');
+        </script>
       </Head>
 
       <main
